d77872cdcb4f4e120e62b63ae5fc14af982c40fa
wangjun43 <wangjun43@baidu.com>
2019-12-26 10:49:43 +0800

    123

---
+++ cmake/log_control.cmake
@@ -0,0 +1,12 @@
+option(LOG0 "LOG0 enable" OFF) #chenke log enable
+option(LOG1 "LOG1 enable" OFF) #shendayun log enable
+option(LOG2 "LOG2 enable" OFF) #wangjun log enable
+option(LOG3 "LOG3 enable" OFF)
+option(LOG4 "LOG4 enable" OFF)
+option(LOG5 "LOG5 enable" OFF)
+option(LOG6 "LOG6 enable" OFF)
+option(LOG7 "LOG7 enable" OFF)
+option(LOG8 "LOG8 enable" OFF)
+option(LOG9 "LOG9 enable" OFF)
+option(LOG10 "LOG10 enable" OFF)
+
